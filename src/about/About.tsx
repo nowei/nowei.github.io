b46c8837600b301285ef0a51092208d7b10e06d0
Andrew Wei <a.nowei42@gmail.com>
2023-11-21 01:42:28 -0800

    some about

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -2,13 +2,18 @@ import React from "react";
 
 function AboutMe() {
   return (
-    <div>
+    <div style={{ width: "50%" }}>
       <p>Hello</p>
       <p>
-        I'm a senior software engineer at a startup in Seattle. What this means
-        is that I make a lot of mistakes really fast and also fix them really
-        fast.
+        I'm a señor software engineer at a startup in Seattle. Being a software
+        engineer just means that I go clickity-clack on my keyboard to move bits
+        in the cloud.
       </p>
+      <p>
+        I spend a lot of time thinking about things and how we get to where we
+        are.
+      </p>
+      <p>I like to volunteer in my free time.</p>
     </div>
   );
 }
