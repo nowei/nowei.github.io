a1f047b9e32a508c36d9b69d5a1d98fd38a99d8a
Andrew Wei <a.nowei42@gmail.com>
2024-06-13 20:41:49 -0700

    [resume] update for teaching application

--- src/writing/blogs/20240601.tsx
+++ src/writing/blogs/20240601.tsx
@@ -7,41 +7,58 @@ function Blog20240601() {
       <hr />
       <h2>It's 10:56 PM.</h2>
       <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;I'm tired, or maybe I think I am. It's kind of
-        weird. I wake up, go to the gym, work, eat, and then sleep. I
+        &nbsp;&nbsp;&nbsp;&nbsp;I'm tired, or maybe I just think I am. It's kind
+        of weird. I wake up, go to the gym, work, eat, and then sleep. I
         occasionally visit friends, but many of them live in Seattle and I'm
         waiting for the light rail station in Lynnwood to open up. Even then,
         it's still faster to drive in many cases, but at least there would be
-        options.
+        more options. Or they live in Bellevue and I need to find the motivation
+        to drive down there.
       </p>
       <p>
         &nbsp;&nbsp;&nbsp;&nbsp;I find it hard to travel far and go places, but
-        at the same time I don't want to stay inside all day and rot away,
-        grinding away at work on one project to the next. It's hard to find a
-        good balance because the entire idea of the 9-to-5 is that you are
-        working with maybe an hour of lunch in between and that you're available
-        to talk to or monitoring things in case something breaks. But the 9-to-5
-        was started in the 1920s and we just kept it alive for all this time. We
-        are arguably the most productive we have ever been in human history, but
-        we hold onto the antiquated idea of working those hours because we've
-        done so in the past. Maybe my problem is that I'm self-aware of the
-        rotting. Maybe my problem is that when I do something, I'm either bored
-        and get tired, or I hyperfixate on finishing something?
+        I should probably just bite the bullet and go. I don't want to stay
+        inside all day and rot away, grinding away at work on one project to the
+        next. It's hard to find a good balance during the work week because the
+        entire idea of the 9-to-5 is that you are working with maybe 30 minutes
+        to an hour of lunch in between. Then the rest of the time you're
+        supposed to be working, be available to talk, join meetings, or monitor
+        things in case something breaks. But the 9-to-5 was started in the 1920s
+        and we just kept it alive for all this time. We are arguably the most
+        productive we have ever been in human history, but we hold onto the
+        antiquated idea of working those hours because that's what we did in the
+        past. Maybe my problem is that I'm aware of the rotting. When I do
+        things, I cycle through hyperfixating on things, then getting tired,
+        then bored? Then I get burnt out if I work too much and question why I'm
+        doing anything or if I should just close up shop and go on a journey of
+        self-discovery.
       </p>
       <p>
         How did the generations before us do it? Maybe they had brighter
-        futures? Did they have a better sense of duty to the work that they do?
-        To the company they spend their limited time on earth? Maybe it's the
-        social security? Maybe inflation wasn't as bad back then? Maybe they
-        could own buy a house and raise a family? Maybe life was not as bleak?
+        futures, higher hopes? Did they have a higher sense of duty to the work
+        that they do? To the company they spend their limited time on earth
+        grinding away for? To the family that they're working to support? Maybe
+        it's reinforcement of societal norms, the fact that previous generations
+        had it worse, therefore we should be grateful for what we have and how
+        we have it? Maybe inflation wasn't as bad back then? Maybe they could
+        own and buy a house and raise a family? Maybe life was not as bleak?
+      </p>
+      <p>
+        Where did the collective shine in our eyes go? The purpose that we are
+        all searching for. Does it actually exist or is it something we made up
+        to cope with the heavy, crushing reality we find ourselves living in
+        (struggling through)?
       </p>
       <p>
         I think I'm okay at what I do, or at least that's what people tell me.
         Maybe I'm complaining too much, but is the intensity of the suffering I
-        feel overexaggerated? But it is how I feel and is it wrong of me to feel
-        that way?
+        feel overexaggerated? I know that I am in a privileged situation in that
+        I can work from home and make money, but this is how I feel and is it
+        wrong of me to feel that way? I guess everyone's perspectives are
+        different and they experience life at their own pace?
       </p>
       <iframe
+        title="How it feels to work"
         style={{ borderRadius: "12px" }}
         src="https://open.spotify.com/embed/track/6D0ihmDU6CQoenaeFtKjkj?utm_source=generator"
         width="100%"
@@ -50,13 +67,94 @@ function Blog20240601() {
         loading="lazy"
       />
       <p>
-        But maybe that's just life. It feels like I'm cycling between feeling
-        alright and feeling general angst about life. Oftentimes I feel like I
-        rely too much on my family, like living at home or them cooking for me.
-        The only real contribution I make is to the bills and the occasional
-        grocery run. Who knows?
+        Maybe that's just how life is. It feels like I'm cycling between feeling
+        alright, that I'm making progress in the life that I live, and feeling
+        general angst; like I'm not doing enough or not where I should be. I
+        might be burnt out, maybe from work, or maybe the monotony of the life
+        that I live. I think there's something wrong with me, but I could also
+        be imagining problems that don't exist. Although to deny one's
+        perception of the world is also a sort of oppression.
+        {/* Oftentimes I feel like I rely too much on my */}
+        {/* family, like living at home or them cooking for me. The only real */}
+        {/* contributions I make are to the bills and the occasional grocery run. */}
+      </p>
+      <p>
+        When people look at your life from the outside, how would they perceive
+        it? Not that it should matter, but what would they think? What would
+        they think about the way that you live your life? Your schedule, your
+        diet, your habits, your quirks, what you stand for and believe in. While
+        it is incredibly reductive; you are, in part, what you do. This misses
+        out on the dreams and aspirations that people have, but what strides are
+        you making towards achieving your goals? If you want to learn a new
+        language, what are you doing to make that happen? Although, in many
+        cases, it is usually only those that are privileged enough to be able to
+        choose what they do. There is usually some level of effort involved; but
+        to be able to choose what you can do, to be the master of your own
+        destiny, is not a privilege that most people have.
+      </p>
+      <p>
+        I guess what I want to say is that if you're trying to be the best
+        version of yourself, what are you doing to get there? How do you get
+        from point A to B and not fall into an endless cycle of dread, boredom,
+        and despair? What are we doing with our lives such that we are improving
+        and making strides towards what we want to do and where we want to be?
+      </p>
+      <p>
+        I think there's something wrong with me. I get overwhelmed. I find it
+        hard to start things because if I start something, I would like to
+        finish it, otherwise I feel like I failed somehow. This is a negative
+        attitude that should probably be addressed, since in the marathon of
+        life, taking the first step is crucial to getting anything done. You
+        might not know how to do something, but you start to learn by taking the
+        plunge, the first step, or whatever distance metaphor people use
+        nowadays. We convince ourselves that it is difficult to do, so we don't
+        even start. We tell ourselves that the limited time we have is better
+        spent one way or another. Or that we're just waiting for X or Y before
+        starting something. This is likely why things like new years resolutions
+        often fail. We predicate starting on an arbitrary milestone that will
+        just pass, like any other day of the year. We tell ourselves that it
+        will be different this time around, that we're cracked the code on how
+        our brains tick. That this time, we will succeed in what we set out to
+        do countless times before.
+      </p>
+      <p>
+        But maybe we should just do it. We might fail. Actually, in all
+        likelihood, we will probably fail, but maybe that's just part of the
+        journey. Human resilience is to try, against whatever odds, however
+        great the probability of failure is. We will not get anything done by
+        sitting around and twiddling our thumbs, waiting for the new year or
+        some vacation or break to do something. We kind of just have to take
+        life by the horns and go for it. It's easy to say that we can't do
+        something when we haven't even tried. It's harder to say that we can't
+        do something after actually trying, since problems at that point are
+        identifiable and addressable. Nothing ventured, nothing gained.
+      </p>
+      <p>
+        Successful people have failed in the past, they have pivoted ideas, they
+        have uprooted their sense of how the world works. Failing is a part of
+        the process. Maybe you give up after a day. Maybe you do it after a
+        month? Who we are as a person generally isn't defined by a single day,
+        but by the life we have lived. With our limited time, every day matters.
+      </p>
+      <p>
+        In any case, I might be burnt out. I might be depressed. I might have
+        crippling social anxiety and I probably have neurodivergent tendencies.
+        I don't think I'm normal. I mean, in this day and age, who is? Maybe my
+        frame of reference is messed up. Everyone considers themselves normal
+        until they learn that they aren't. If it makes sense to you, it might
+        not make sense to other people, at least not in the same way it does to
+        you. It's kind of hard to put it into words. A word for not being able
+        to be described is indescribable, but this isn't exactly that. Since
+        while it is hard to define, it is not impossible to define.
+      </p>
+      <p>
+        Maybe this is just the life that I live. In the universe of possible
+        outcomes, maybe there is nothing wrong with me. But then why does it
+        feel like there's something wrong with me? It's not my goal in life to
+        stand out and be different, but I don't think I'm crazy.
       </p>
       <iframe
+        title="It do be wild"
         style={{ borderRadius: "12px" }}
         src="https://open.spotify.com/embed/track/0MebeWX9y2irXrCCNcK58i?utm_source=generator&theme=0"
         width="100%"
@@ -64,6 +162,34 @@ function Blog20240601() {
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
       />
+      <p>
+        I always wondered why people rewatch TV Shows, since there's more
+        content available than any one person can consume in their lifetime. Why
+        would they go back and watch something that they've already seen? From
+        what I understand, people did it because those shows are familiar and
+        could act as some sort of baseline they could return to whenever they
+        wanted to, where they knew exactly what was going to happen and remind
+        them of the good times that they had when they first watched it. Perhaps
+        it even provided them some solace and stability in the turbulent world
+        that we live in.
+      </p>
+      <p>
+        But I also play games, which in a sense is somewhat similar, we might
+        return to the games that we do because there's a sense of comfort or
+        security in it. Or you can play with friends and unwind and focus your
+        mind on something else for a few hours. I also started playing Hades and
+        then eventually Hades 2 and realized that it's kind of similar, the fact
+        that we play the same game over and over again is similar to watching a
+        show over and over again. It might not be the best use of time, but it
+        feels comfortable because you know exatly what will happen.
+      </p>
+      <p>
+        I started going to the gym more since November, but then I learned a few
+        things about working out. If the goal is to build muscle, then it's more
+        effective to work out to failure every time than trying to get more
+        reps. But if the goal is to build endurance, then you should try to do
+        more reps to failure.
+      </p>
     </div>
   );
 }
